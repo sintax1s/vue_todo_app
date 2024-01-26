78affa350d9be91c48a14a3ed45aed22a510d800
Vladyslav Atanov <gibepromt@gmail.com>
2024-01-26 14:05:10 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue3-todo-app/',
+  base: '/vue3_todo_app/',
   plugins: [vue()],
   resolve: {
     alias: {
